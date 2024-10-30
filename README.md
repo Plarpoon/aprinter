25804d61152419eb2edf3c64e35c6d83d135a742
Alessandro Suha <github@plarp.net>
2024-10-30 14:51:02 +0100

    feat: add MIT License

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# trak-automatic-printer
+# aprinter
 
-dependencies: required openssl-devel
+Automated printing tool from the terminal that allows you to pass any printable file directly to a CUPS server
+
+---
+**build dependencies**: openssl-devel (tested with version 3.2.2)
--- src/manual.rs
+++ src/manual.rs
@@ -1,12 +1,12 @@
 pub const MANUAL: &str = r#"
 NAME
-    trak-aprinter - A command-line tool for submitting print jobs to a remote CUPS server via IPP.
+    aprinter - A command-line tool for submitting print jobs to a remote CUPS server via IPP.
 
 SYNOPSIS
-    trak-aprinter [-url server] [-pname printer] [-delay delay] [-files files...]
+    aprinter [-url server] [-pname printer] [-delay delay] [-files files...]
 
 DESCRIPTION
-    trak-aprinter is a command-line tool designed to submit print jobs to a remote CUPS server via
+    aprinter is a command-line tool designed to submit print jobs to a remote CUPS server via
     the IPP protocol. It allows users to specify the CUPS server URL, printer name (mandatory), and
     list of files to be printed, with an optional delay between each print job.
 
@@ -29,10 +29,10 @@ OPTIONS
 
 EXAMPLES
     To print a single file to a printer on a remote server with a 5-second delay:
-        trak-aprinter -url "192.168.3.18" -pname "TA_UTAX_2507ci_" -delay 5 -files "file1.txt"
+        aprinter -url "192.168.3.18" -pname "TA_UTAX_2507ci_" -delay 5 -files "file1.txt"
 
     To print multiple files to a printer on localhost with a 1-second delay (default):
-        trak-aprinter -pname "MyPrinter" -files "file1.txt" "file2.pdf"
+        aprinter -pname "MyPrinter" -files "file1.txt" "file2.pdf"
 
 EXIT STATUS
     The program exits with status 0 on success and prints any errors encountered during execution.
